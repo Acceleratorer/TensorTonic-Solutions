265720f81d212a71423d62ea9ea3936301c224b7
Tan Le <noobtan77@gmail.com>
2026-03-15 00:30:11 +0700

    Add solution for chi2-independence

---
+++ chi2-independence/chi2-independence.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def chi2_independence(C):
+    """
+    Compute chi-square test statistic and expected frequencies.
+    """
+    C = np.asarray(C, dtype=float)
+
+    row_totals = np.sum(C, axis=1)
+    col_totals = np.sum(C, axis=0)
+    total = np.sum(C)
+
+    expected = np.outer(row_totals, col_totals) / total
+    chi2 = np.sum((C - expected) ** 2 / expected)
+
+    return float(chi2), expected
